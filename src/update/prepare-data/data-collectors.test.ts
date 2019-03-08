fa2c9ee898a718d0e79ed841a3f3dd28376db340
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-03-09 00:02:42 +0100

    Added CI

--- src/update/prepare-data/data-collectors.test.ts
+++ src/update/prepare-data/data-collectors.test.ts
@@ -1,17 +1,26 @@
+
 process.env.dbName = 'cloudkeeper'
 process.env.dbHost = 'cloudkeeper.cluster-ckbplh6wfiop.eu-central-1.rds.amazonaws.com'
 process.env.dbUser = 'cloudkeeper'
 process.env.dbPassword = 'ExH58GwqZBnCV49MqWcV'
 
 // eslint-disable-next-line import/first
+import { getConnection } from '../../db/db'
+// eslint-disable-next-line import/first
 import { getTotals } from './data-collectors'
 
-describe('collectors', async () => {
+describe('collectors', () => {
   jest.setTimeout(30000)
 
   test('totals', async () => {
     const totals = await getTotals('emarketeer')
 
     expect(totals.invocations).toBeTruthy()
+  })
+
+
+  afterAll(async () => {
+    const connection = await getConnection()
+    await connection.close()
   })
 })
