367286e827858ff27d2d8313506bac13bbd64b7e
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-03-08 23:43:22 +0100

    Added CI

--- src/update/lambda-stats/update-tenant-lambda-stats.test.ts
+++ src/update/lambda-stats/update-tenant-lambda-stats.test.ts
@@ -1,23 +1,23 @@
-import { handler } from './update-tenant-lambda-stats'
-
 process.env.dbName = 'cloudkeeper'
 process.env.dbHost = 'cloudkeeper.cluster-ckbplh6wfiop.eu-central-1.rds.amazonaws.com'
 process.env.dbUser = 'cloudkeeper'
 process.env.dbPassword = 'ExH58GwqZBnCV49MqWcV'
+process.env.AWS_REGION = 'eu-central-1'
+process.env.finishedTopic = 'arn:aws:sns:eu-central-1:537011205135:dev-finished-updating-lambda-stats'
 
-describe('Token Methods', async () => {
-  jest.setTimeout(30000)
+// eslint-disable-next-line import/first
+import { handler } from './update-tenant-lambda-stats'
 
+describe('Update Lambda Stats', async () => {
+  jest.setTimeout(60000)
+
+  //
   test('happy path', async () => {
-    try {
-      await handler({
-        accessKey: 'AKIAJI7Y7EUA4WTJYTUA',
-        region: 'eu-west-1',
-        secretKey: '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',
-        tenantId: 'emarketeer',
-      })
-    } catch (err) {
-      console.log(err)
-    }
+    await handler({
+      accessKey: 'AKIAJI7Y7EUA4WTJYTUA',
+      region: 'eu-west-1',
+      secretKey: '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',
+      tenantId: 'emarketeer',
+    })
   })
 })
--- src/update/prepare-data/data-collectors.test.ts
+++ src/update/prepare-data/data-collectors.test.ts
@@ -1,21 +1,17 @@
-import { getTotals } from './data-collectors'
-
 process.env.dbName = 'cloudkeeper'
 process.env.dbHost = 'cloudkeeper.cluster-ckbplh6wfiop.eu-central-1.rds.amazonaws.com'
 process.env.dbUser = 'cloudkeeper'
 process.env.dbPassword = 'ExH58GwqZBnCV49MqWcV'
 
+// eslint-disable-next-line import/first
+import { getTotals } from './data-collectors'
 
 describe('collectors', async () => {
   jest.setTimeout(30000)
 
   test('totals', async () => {
-    try {
-      const totals = await getTotals('emarketeer')
+    const totals = await getTotals('emarketeer')
 
-      expect(totals.totals.invocations).toBeTruthy()
-    } catch (err) {
-      console.log(err)
-    }
+    expect(totals.invocations).toBeTruthy()
   })
 })
