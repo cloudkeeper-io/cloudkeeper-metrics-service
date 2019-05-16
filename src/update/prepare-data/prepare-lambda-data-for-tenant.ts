177be9f6ec2bc12ccf73d0f5917f0950da2bd8bb
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-05-16 22:37:19 +0200

    Added costs data update

--- src/update/prepare-data/prepare-lambda-data-for-tenant.ts
+++ src/update/prepare-data/prepare-lambda-data-for-tenant.ts
@@ -1,10 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as AWS from 'aws-sdk'
 import {
-  getMostErrorsLambdas,
-  getMostExpensiveLambdas,
-  getMostInvokedLambdas,
-  getSlowestLambdas,
   getTotals,
 } from './lambda-data-collectors'
 
@@ -17,39 +13,14 @@ export const handler = async (event) => {
     console.log(`Preparing data for ${tenantId}`)
 
     const totals = await getTotals(tenantId, 1)
-
-    const slowestLambdas = await getSlowestLambdas(tenantId, 1)
-
-    const mostInvokedLambdas = await getMostInvokedLambdas(tenantId, 1)
-
-    const mostErrorsLambdas = await getMostErrorsLambdas(tenantId, 1)
-
-    const mostExpensiveLambdas = await getMostExpensiveLambdas(tenantId, 1)
-
     const last30DaysTotals = await getTotals(tenantId, 30, true)
-
-    const last30DaysSlowestLambdas = await getSlowestLambdas(tenantId, 30, true)
-
-    const last30DaysMostInvokedLambdas = await getMostInvokedLambdas(tenantId, 30, true)
-
-    const last30DaysMostErrorsLambdas = await getMostErrorsLambdas(tenantId, 30, true)
-
-    const last30DaysMostExpensiveLambdas = await getMostExpensiveLambdas(tenantId, 30, true)
 
     const data = {
       last24Hours: {
         totals,
-        slowestLambdas,
-        mostInvokedLambdas,
-        mostErrorsLambdas,
-        mostExpensiveLambdas,
       },
       last30Days: {
         totals: last30DaysTotals,
-        slowestLambdas: last30DaysSlowestLambdas,
-        mostInvokedLambdas: last30DaysMostInvokedLambdas,
-        mostErrorsLambdas: last30DaysMostErrorsLambdas,
-        mostExpensiveLambdas: last30DaysMostExpensiveLambdas,
       },
     }
 
