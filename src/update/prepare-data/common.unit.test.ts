d1ee64ad4c7086291e543d59ac4d93d2138c76f6
ayankovsky <p44gr10@gmail.com>
2019-08-24 22:47:59 +0200

    Fixed dates

--- src/update/prepare-data/common.unit.test.ts
+++ src/update/prepare-data/common.unit.test.ts
@@ -3,7 +3,7 @@ import { fillEmptyDataPoints } from './common'
 
 describe('test common functions', () => {
   test('fillEmptyDataPoints - 30 days - empty datapoints', async () => {
-    const dataPoints = fillEmptyDataPoints([], true, 30, { count: 0 })
+    const dataPoints = fillEmptyDataPoints([], true, 29, { count: 0 })
     expect(dataPoints.length).toBe(30)
   })
 
@@ -14,13 +14,13 @@ describe('test common functions', () => {
       dateTime: DateTime.utc().startOf('day').minus({ days: 5 }).toJSDate(),
     }]
 
-    const dataPoints = fillEmptyDataPoints(dataPointsWithSomeData, true, 30, { count: 0 })
+    const dataPoints = fillEmptyDataPoints(dataPointsWithSomeData, true, 29, { count: 0 })
     expect(dataPoints.length).toBe(30)
   })
 
   test('fillEmptyDataPoints - 24 hours', async () => {
     const dataPoints = fillEmptyDataPoints([], false, 1, { count: 0 })
-    expect(dataPoints.length).toBe(24)
+    expect(dataPoints.length).toBe(25)
   })
 
   test('fillEmptyDataPoints - 24 hours', async () => {
@@ -31,6 +31,6 @@ describe('test common functions', () => {
     }]
 
     const dataPoints = fillEmptyDataPoints(dataPointsWithSomeData, false, 1, { count: 0 })
-    expect(dataPoints.length).toBe(24)
+    expect(dataPoints.length).toBe(25)
   })
 })
